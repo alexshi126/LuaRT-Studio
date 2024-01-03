6ced1ad3e569b4e92d71259e31b46f1454d04230
Samir Tine <samir.tine@luart.org>
2024-01-03 18:56:51 +0100

    Fixed: tables with metatables are now pretty printed correctly

--- lualibs/pretty.lua
+++ lualibs/pretty.lua
@@ -1,5 +1,5 @@
 -- | LuaRT - A Windows programming framework for Lua
--- | Luart.org, Copyright (c) Tine Samir 2023.
+-- | Luart.org, Copyright (c) Tine Samir 2024.
 -- | See Copyright Notice in LICENSE.TXT
 -- |------------------------------------------------
 -- | pretty.lua | Pretty print a LuaRT value
@@ -14,21 +14,21 @@ function pretty(value, isobj, name)
   local result = ""
   
   if t == "string" then
-    if value:find("[\r\n]") then
-      result = "[["..value:gsub("\\","/").."]]"
+    if value:ufind("[\r\n]") then
+      result = "[["..value:gusub("\\","/").."]]"
     else
-      result = '"'..value:gsub("\\","/"):gsub('"', '\\"')..'"'
+      result = '"'..value:gusub("\\","/")..'"'
     end
   elseif t == "number" or t == "boolean" then
     result = tostring(value)
   elseif t == "function" then
     if isobj then
       local meta = getmetatable(isobj)
-      result = name:find("^[gs]et_") and (meta and meta.__type and isobj[name:find("^[gs]et_")] or "property") or (meta and (meta.__type or meta.__mt) and "method" or "function")
+      result = name:ufind("^[gs]et_") and (meta and meta.__type and isobj[name:ufind("^[gs]et_")] or "property") or (meta and (meta.__type or meta.__mt) and "method" or "function")
     else
       result = t
     end
-  elseif name == nil and (t == "table" or mt) then
+  elseif name == nil and t ~= "userdata" and (t == "table" or mt) then
     local out = {}
     local obj = type(mt) == "table" and ((mt.__type or mt.__properties) or (type(mt.__name) == "string"))
     result = "{"
@@ -45,7 +45,7 @@ function pretty(value, isobj, name)
       if obj then           
         prop, n = k:gsub("^set_", "get_"):gsub("^[gs]et_", "")
       end
-      local space = string.rep(" ", 14-string.len(prop)).."= "
+      local space = string.len(prop) < 6 and "\t\t\t= " or "\t\t= "      
       if out[prop] == nil then
         if obj == true then
           result = result.."\n  "..prop..space..pretty(v, value, k)..","
@@ -57,13 +57,16 @@ function pretty(value, isobj, name)
     end
     n = 0
     if obj ~= false and obj ~= true then
+      local isobject = mt.__name == "Object"
       for k, v in pairs(obj) do
         local prop, n = k:gsub("^set_", "get_"):gsub("^[gs]et_", "")
-        if out[prop] == nil then
-          local space = string.rep(" ", 14-string.len(prop)).."= "
+        local space = string.len(prop) < 6 and "\t\t\t= " or "\t\t= "      
+        if isobject and out[prop] == nil then
+          result = result.."\n  "..prop..space..pretty(v, value, k)..","
+        elseif out[prop] == nil then
           result = result.."\n  "..prop..space..pretty(n and value[prop] or v, value, k)..","
-          out[prop] = v
         end
+        out[prop] = v
       end
     end
     result = result.."}"
