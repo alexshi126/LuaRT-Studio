54cfc7d754415e80d3cbec43cf9dd9a8194ac124
Samir Tine <samir.tine@luart.org>
2024-01-26 22:38:51 +0100

    Fixed: Running scripts with arguments sets now the `arg` table correctly

--- interpreters/desktop.lua
+++ interpreters/desktop.lua
@@ -36,7 +36,7 @@ return {
     if params ~= nil then
         params = params:gsub("[^%s]+", function(param) return '\\"'..param..'\\"' end)
     end
-    local cmd = '"'..exe..'" -e "sys.cmd(\'\\"\\"'..exe:gsub("\\", "/")..'\\" \\"'..(filepath):gsub("\\", "/")..'\\"'..(params and '\\" '..params or '')..[[', false)"]]
+    local cmd = '"'..exe..'" -e "sys.cmd(\'\\"\\"'..exe:gsub("\\", "/")..'\\" \\"'..(filepath):gsub("\\", "/")..'\\" '..(params and params..'\\"' or '')..[[', false)"]]
     local envcpath = "LUA_CPATH"
     local envlpath = "LUA_PATH"
     if version then
